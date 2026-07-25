9dd0610dd6c4288e2664bf5ffb6bc9df4608b3a8
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-25 20:58:28 +0530

    refactor: enhance main.py to include website viewing functionality

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from storage import add_passwords
+from storage import add_passwords, view_websites
 
 add_passwords("github.com", "shreyesh", "abc123")
 print("Yo, homie, your password is added!!!")
+
+print("\n")
+
+view_websites()
