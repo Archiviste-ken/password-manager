0a1448bfa6dc283d45c3bf88bc280a204d25b818
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-25 20:16:49 +0530

    refactor: streamline add_passwords function for better readability

--- storage.py
+++ storage.py
@@ -1,6 +1,5 @@
 import json
 
-
 def load_passwords():
     with open("passwords.json","r") as file:
         passwords = json.load(file)
@@ -11,6 +10,21 @@ def save_passwords(passwords):
     
     with open("passwords.json","w") as file:
         json.dump(passwords, file, indent = 4)
+    
+    
+def add_passwords(website, username, password):
+    
+    passwords  = load_passwords()
+    
+    password = {
+        "website": website,
+        "username": username,
+        "password": password
+    }
+    
+    passwords.append(password)
+    
+    save_passwords(passwords)
     
     
         
