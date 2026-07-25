af30536a8477af11c85d8fafe4f69409ef25d97a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-26 00:05:12 +0530

    feat: update search functionality to use search_password and add delete_password

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from storage import add_passwords, view_websites, search_websites
+from storage import add_passwords, view_websites, search_password, delete_password
 
 add_passwords("github.com", "shreyesh", "abc123")
 print("Yo, homie, your password is added!!!")
@@ -7,4 +7,6 @@ print("\n")
 
 view_websites()
 
-search_websites("github.com")
+search_password("github.com")
+
+delete_password(1)
