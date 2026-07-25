61d794ab0f8a84e4ecc87f7b504f45b1279817cd
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-25 21:41:33 +0530

    feat: add search functionality for websites in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from storage import add_passwords, view_websites
+from storage import add_passwords, view_websites, search_websites
 
 add_passwords("github.com", "shreyesh", "abc123")
 print("Yo, homie, your password is added!!!")
@@ -6,3 +6,5 @@ print("Yo, homie, your password is added!!!")
 print("\n")
 
 view_websites()
+
+search_websites("github.com")
