8cff18c03c3e20de95c0935c3192aa4daf8df436
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-26 23:43:51 +0530

    feat: add password generation functionality and update delete_password usage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from storage import add_passwords, view_websites, search_password, delete_password
+from password_utils import generate_password
 
 add_passwords("github.com", "shreyesh", "abc123")
 print("Yo, homie, your password is added!!!")
@@ -10,3 +11,7 @@ view_websites()
 search_password("github.com")
 
 delete_password(1)
+
+password = generate_password(12)
+
+print(password)
